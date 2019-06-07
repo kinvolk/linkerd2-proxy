0824a9b9166ff17fe6446f1ea32d906b69f8d650
Kai Lüke <kai@kinvolk.io>
2019-06-07 12:28:02 +0200

    Separate inbound/outbound profiling runs
    Limits the profiling information to a specific
    task (inbound/outbound) in the program which
    can then be optimized.
    
    The files will be created in a subdirectory.

--- profiling/profiling-build.sh
+++ profiling/profiling-build.sh
@@ -3,5 +3,7 @@ set -x
 set -o errexit
 set -o nounset
 set -o pipefail
-rm ./target/release/profiling-* || true
+PROFDIR=$(dirname "$0")
+cd "$PROFDIR"
+rm ../target/release/profiling-* || true
 RUSTFLAGS="-C debuginfo=2 -C lto=off" cargo test --release --no-run profiling_setup  # try lto=thin or =fat if they don't make perf miss calls
